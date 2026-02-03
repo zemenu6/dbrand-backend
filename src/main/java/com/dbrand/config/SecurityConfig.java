28aec31103239b15dc9f2ee19b961b6c358ca1ae
Zemenu | Software Engineering Student <daggydaggy19@gmail.com>
2026-02-03 15:29:11 +0300

    Update SecurityConfig.java

--- src/main/java/com/dbrand/config/SecurityConfig.java
+++ src/main/java/com/dbrand/config/SecurityConfig.java
@@ -68,7 +68,8 @@ public class SecurityConfig {
             "http://localhost:8081",
             "http://127.0.0.1:3000",
             "http://127.0.0.1:8081",
-            "https://dmsbrand.netlify.app"
+            "https://dmsbrand.netlify.app",
+            "https://adminpagedbbrand.netlify.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
